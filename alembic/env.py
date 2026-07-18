64fdff143af3e637d198bcb604a2e1b2509ec34a
hiteshchundi <hitzhraj96@gmail.com>
2026-07-18 10:17:27 +0530

    feat: configure Alembic and create initial database schema migration

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,12 @@ from alembic import context
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+from app.core.config import settings
+
+config.set_main_option(
+    "sqlalchemy.url",
+    settings.DATABASE_URL,
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -18,8 +24,9 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from app.db.base import Base
 
+target_metadata = Base.metadata
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
